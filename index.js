87cc1371876751e0887e62de3064e961ec7ee86c
Leda <ledathemis@protonmail.com>
2022-06-24 19:58:44 +0000

    rewrite in express

--- index.js
+++ index.js
@@ -1,44 +1,24 @@
-const http = require('http');
-const fs = require('fs/promises');
+const express = require('express');
+const app = express();
 
 const port = 8080;
 
-const readHTML = async (filePath) => {
-  try {
-    const data = await fs.readFile(filePath, { encoding: 'utf8' });
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-const respond = (response, filePath) => {
-  response.statusCode = 200;
-  response.setHeader('Content-Type', 'text/html');
-  readHTML(filePath).then((html) => {
-    response.end(html);
-  });
-};
-
-const server = http.createServer((req, res) => {
-  const url = new URL(req.url, `http://${req.headers.host}`);
-  const path = url.pathname;
-  switch (path) {
-    case '/':
-      respond(res, 'index.html');
-      break;
-    case '/about':
-      respond(res, 'about.html');
-      break;
-    case '/contact-me':
-      respond(res, 'contact-me.html');
-      break;
-    default:
-      respond(res, '404.html');
-      return;
-  }
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
 });
 
-server.listen(port, () => {
+app.get('/about', (req, res) => {
+  res.sendFile(`${__dirname}/about.html`);
+});
+
+app.get('/contact-me', (req, res) => {
+  res.sendFile(`${__dirname}/contact-me.html`);
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(`${__dirname}/404.html`);
+});
+
+app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
